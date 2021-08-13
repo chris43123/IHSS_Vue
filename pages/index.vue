ea9e21dbc7f8b6575bbc7cc98472946c3dfcd765
chris43123 <46337972+chris43123@users.noreply.github.com>
2021-08-12 21:22:22 -0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="info != undefinded">
+    {{info.base_experience}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default 
+{
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/gengar')
+      .then(response => (this.info = response.data))
+  }
+}
+</script>
